31d4a3015e869d17d562f12a5cbd49418da3f308
Oskar Gewalli <oskar@gewalli.se>
2016-07-03 09:36:00 +0200

    Added project and test project

---
+++ SweIdNum/PersonalIdentityNumber.fs
@@ -0,0 +1,8 @@
+﻿namespace SweIdNum
+
+
+module PersonalIdentityNumber=
+    [<CompiledName("IsValid")>]
+    let isValid (pin:string)= false
+    [<CompiledName("Format")>]
+    let format (pin:string)= ""
---
+++ Tests/rccvast_persnr_fejk.fs
@@ -0,0 +1,4 @@
+﻿module PersonNummerFejk
+open FSharp.Data
+
+
