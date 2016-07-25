9cba985b07edfdd89c11612159417fd4f7f3fd65
Oskar Gewalli <oskar@gewalli.se>
2016-07-25 12:33:41 +0200

    travis and appveyor config

---
+++ build.fsx
@@ -0,0 +1,60 @@
+#I "packages/FAKE/tools/"
+#r "FakeLib.dll"
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+open Fake
+open Fake.Testing
+let libDir  = "./SweIdNum/bin/Debug/"
+let testDir   = "./Tests/bin/Debug/"
+
+let solutionFile  = "SweIdNum.sln"
+
+Target "build" (fun _ ->
+    !! solutionFile
+    |> MSBuildDebug "" "Rebuild"
+    |> ignore
+)
+
+Target "build_release" (fun _ ->
+    !! solutionFile
+    |> MSBuildRelease "" "Rebuild"
+    |> ignore
+)
+
+Target "test" (fun _ ->  
+    !! (Path.Combine(testDir, "*Tests*.dll"))
+        |> NUnit3 (fun p -> 
+            {p with
+                ShadowCopy = false; 
+                ResultSpecs = [testDir + "TestResults.xml"]})
+)
+
+Target "pack" (fun _ ->
+    Paket.Pack(fun p ->
+        { p with
+            OutputPath = libDir})
+)
+
+Target "clean" (fun _ ->
+    CleanDirs [libDir; testDir]
+)
+
+Target "push" (fun _ ->
+    Paket.Push(fun p ->
+        { p with
+            WorkingDir = libDir })
+)
+
+
+Target "install" DoNothing
+
+"build_release"
+    ==> "pack"
+
+"build"
+    ==> "test"
+
+
+RunTargetOrDefault "build"
